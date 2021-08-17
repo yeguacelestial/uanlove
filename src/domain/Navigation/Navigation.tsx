202d90107247d5cbcd4da482a03eafdcc7761116
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-17 14:16:34 -0500

    Created minimal tab-based navigation

--- src/domain/Navigation/Navigation.tsx
+++ src/domain/Navigation/Navigation.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '@screens/Home';
+import Profile from '@screens/Profile';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 const Navigation: React.FC = () => {
   return (
     <NavigationContainer>
       <Navigator>
         <Screen component={Home} name="Home" />
+        <Screen component={Profile} name="Profile" />
       </Navigator>
     </NavigationContainer>
   );
