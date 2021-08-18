ffd1c67644273c2471f6024787da1e7035d8984e
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-17 20:29:16 -0500

    Changed content padding bottom

--- src/components/ScrollView/ScrollView.tsx
+++ src/components/ScrollView/ScrollView.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { ScrollView as ScrollViewBase } from 'react-native';
+import {
+  ScrollView as ScrollViewBase,
+  ScrollViewProps as ScrollViewBaseProps
+} from 'react-native';
 
-const ScrollView = (props: any) => {
+export type ScrollViewProps = ScrollViewBaseProps;
+
+const ScrollView: React.FC<ScrollViewProps> = ({
+  style,
+  ...props
+}: ScrollViewProps) => {
   return (
     <ScrollViewBase
-      style={{
-        paddingBottom: 60
-      }}
-    >
-      {props.children}
-    </ScrollViewBase>
+      style={[
+        style,
+        // eslint-disable-next-line react-native/no-inline-styles
+        {
+          paddingBottom: 110
+        }
+      ]}
+      {...props}
+    />
   );
 };
 
