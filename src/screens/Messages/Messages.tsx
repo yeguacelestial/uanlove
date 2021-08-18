8580ea52c85b09aa935c3ee40cbb7fe1c4da52b5
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-17 19:42:18 -0500

    Created Messages screen

---
+++ src/screens/Messages/Messages.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import ScrollView from '@components/ScrollView/ScrollView';
+
+const Messages: React.FC = () => {
+  return (
+    <ScrollView>
+      <Text>Messages Screen</Text>
+    </ScrollView>
+  );
+};
+
+export default Messages;
