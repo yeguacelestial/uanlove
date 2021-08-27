6ec85dd045d6b972b3deaec991642089c6e6e7d7
Mauricio Meléndez <mauhylian@gmail.com>
2021-08-27 16:33:24 -0500

    :nail_care: Enabled EStyleSheet

--- pride/src/domain/App/App.tsx
+++ pride/src/domain/App/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Navigation from '@domain/Navigation';
 import { AuthProvider } from '@context/Auth';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+EStyleSheet.build();
 
 const App: React.FC = () => {
   return (
