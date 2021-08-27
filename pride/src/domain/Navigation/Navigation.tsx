725b03d194792c41b6f22fc16e7c078880f27a27
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-26 20:25:24 -0500

    :bug: Fixed profile navigation warning

--- pride/src/domain/Navigation/Navigation.tsx
+++ pride/src/domain/Navigation/Navigation.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { useAuth } from '@context/Auth/hooks';
 
 import AppNavigator from '@navigation/AppNavigator';
 import AuthNavigator from '@navigation/AuthNavigator';
 
+const { Navigator, Screen } = createNativeStackNavigator();
+
 const Navigation: React.FC = () => {
   const { authenticated } = useAuth();
 
   return (
     <NavigationContainer>
-      {authenticated ? <AppNavigator /> : <AuthNavigator />}
+      <Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        {authenticated ? (
+          <Screen component={AppNavigator} name="app-navigator" />
+        ) : (
+          <Screen component={AuthNavigator} name="auth-navigator" />
+        )}
+      </Navigator>
     </NavigationContainer>
   );
 };
