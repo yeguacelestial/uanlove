bb211cf27d92fa2b463f3ceb8afe4bada83fb8db
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-27 13:59:11 -0500

    :art: Updated ProfileScreen layout

--- pride/src/screens/profile/ProfileScreen/ProfileScreen.tsx
+++ pride/src/screens/profile/ProfileScreen/ProfileScreen.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { useAuth } from '@context/Auth/hooks';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useAuth } from '@context/Auth/hooks';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import ScreenView from '@components/ScreenView';
 
 // TODO: Move this to navigator file.
 type ProfileNavigatorParamList = {
@@ -25,19 +26,37 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({
   navigation
 }: ProfileScreenProps) => {
   const auth = useAuth();
-
   return (
-    <View>
-      <Text>Profile Screen</Text>
-      <Button
-        title="Detail"
-        onPress={() => navigation.push('profile-detail')}
-      />
-      <Button title="Edit" onPress={() => navigation.push('profile-edit')} />
-      <Button title="Settings" onPress={() => navigation.push('settings')} />
-      <Button title="Sign Out" onPress={() => auth.setAuthenticated(false)} />
-    </View>
+    <ScreenView style={styles.root}>
+      <View style={styles.card}>
+        <Text>Card</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button
+          title="Detail"
+          onPress={() => navigation.push('profile-detail')}
+        />
+        <Button title="Edit" onPress={() => navigation.push('profile-edit')} />
+        <Button title="Settings" onPress={() => navigation.push('settings')} />
+        <Button title="Sign Out" onPress={() => auth.setAuthenticated(false)} />
+      </View>
+    </ScreenView>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {},
+  card: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1
+  },
+  actions: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    justifyContent: 'center',
+    flexDirection: 'row'
+  }
+});
 
 export default ProfileScreen;
