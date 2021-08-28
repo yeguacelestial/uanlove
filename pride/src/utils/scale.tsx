882b42ee047e1b2567dfb15bdbd52982f5e627ed
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-27 22:13:17 -0500

    :fire: Removed scale utility function

--- pride/src/utils/scale.tsx
+++
@@ -1,9 +0,0 @@
-import { Dimensions } from 'react-native';
-const { width } = Dimensions.get('window');
-
-//Guideline sizes are based on standard ~5" screen mobile device
-const guidelineBaseWidth = 350;
-
-const scale = (size: number): number => (width / guidelineBaseWidth) * size;
-
-export default scale;
