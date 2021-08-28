21c91bd045973c530d16a6fd495675f75d06d9c4
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-08-27 20:18:25 -0500

    :fire: Removed unused import

--- pride/src/domain/App/App.tsx
+++ pride/src/domain/App/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import Navigation from '@domain/Navigation';
 import { AuthProvider } from '@context/Auth';
-import EStyleSheet from 'react-native-extended-stylesheet';
-
-EStyleSheet.build();
 
 const App: React.FC = () => {
   return (
