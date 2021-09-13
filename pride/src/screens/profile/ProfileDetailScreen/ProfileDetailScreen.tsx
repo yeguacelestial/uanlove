a836bae9182a76582c5f1af13584714c86892d11
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-09-12 20:01:12 -0500

    :art: Added hidden StatusBar to ProfileDetailScreen

--- pride/src/screens/profile/ProfileDetailScreen/ProfileDetailScreen.tsx
+++ pride/src/screens/profile/ProfileDetailScreen/ProfileDetailScreen.tsx
@@ -3,6 +3,7 @@ import UserCardDetail from '@components/UserCardDetail';
 import useAuth from '@hooks/useAuth';
 import { Text, View } from 'react-native';
 import { ProfileDetailScreenProps } from '../props';
+import { StatusBar } from 'expo-status-bar';
 
 const ProfileDetailScreen: React.FC<ProfileDetailScreenProps> = ({
   route: { params },
@@ -20,17 +21,24 @@ const ProfileDetailScreen: React.FC<ProfileDetailScreenProps> = ({
     );
 
   return (
-    <UserCardDetail
-      age={user.age}
-      description={user.description}
-      initialPicture={initialPicture}
-      name={user.name}
-      pictures={[
-        'https://image.shutterstock.com/image-photo/head-shot-portrait-smiling-middle-600w-1339318991.jpg',
-        'https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-600w-1640944705.jpg'
-      ]}
-      onPressExit={() => navigation.goBack()}
-    ></UserCardDetail>
+    <View
+      style={{
+        flex: 1
+      }}
+    >
+      <StatusBar hidden />
+      <UserCardDetail
+        age={user.age}
+        description={user.description}
+        initialPicture={initialPicture}
+        name={user.name}
+        pictures={[
+          'https://image.shutterstock.com/image-photo/head-shot-portrait-smiling-middle-600w-1339318991.jpg',
+          'https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-600w-1640944705.jpg'
+        ]}
+        onPressExit={() => navigation.goBack()}
+      ></UserCardDetail>
+    </View>
   );
 };
 
