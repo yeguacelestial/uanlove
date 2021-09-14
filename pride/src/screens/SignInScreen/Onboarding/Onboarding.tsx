a086379f22bcf5600d5171728694f2e88cea8e13
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-09-14 16:32:37 -0500

    :sparkles: Created Onboarding component with pagination and color interpolation

---
+++ pride/src/screens/SignInScreen/Onboarding/Onboarding.tsx
@@ -0,0 +1,250 @@
+// @refresh reset
+import Button3D from '@components/Button3D';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent
+} from 'react-native-gesture-handler';
+import Animated, {
+  interpolateColor,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring
+} from 'react-native-reanimated';
+import { ms, ScaledSheet } from 'react-native-size-matters';
+
+export interface OnboardingProps {
+  messages: Array<{
+    title: string;
+    subtitle?: string;
+  }>;
+  topColors: string[];
+  bottomColors: string[];
+  onSignInPress?: () => void;
+}
+
+const MAX_ROTATION = 10;
+
+// TODO: Fix top background rotation.
+// TODO: Add page indicator.
+// TODO: Refactor component.
+// TODO: Add color interpolation.
+
+type ContextType = {
+  startRotate: number;
+  startX: number;
+};
+
+const Onboarding: React.FC<OnboardingProps> = ({
+  messages,
+  topColors,
+  bottomColors,
+  onSignInPress
+}: OnboardingProps) => {
+  const [width, setWidth] = useState(Dimensions.get('screen').width);
+  useEffect(() => {
+    setWidth(Dimensions.get('screen').width);
+  }, []);
+
+  const index = useSharedValue(0);
+  const rotate = useSharedValue(MAX_ROTATION);
+  const x = useSharedValue(0);
+
+  const panGestureHandler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart: (_, context) => {
+      context.startX = x.value;
+      context.startRotate = rotate.value;
+    },
+    onActive: (e, context) => {
+      x.value = context.startX + e.translationX;
+
+      /*
+      const angle = context.startRotate + e.translationX * 0.08;
+      rotate.value = Math.min(Math.max(angle, -MAX_ROTATION), MAX_ROTATION);
+      */
+    },
+    onEnd: e => {
+      if (e.translationX < 0) {
+        index.value =
+          index.value === messages.length - 1 ? index.value : index.value + 1;
+      } else {
+        index.value = index.value === 0 ? index.value : index.value - 1;
+      }
+
+      x.value = withSpring(-width * index.value);
+
+      //rotate.value = withSpring(0);
+    }
+  });
+
+  const topStyle = useAnimatedStyle(() => {
+    const inputRange = messages.map((_, index) => -width * index).reverse();
+    const bgColor = interpolateColor(x.value, inputRange, topColors);
+
+    return {
+      backgroundColor: bgColor,
+      transform: [
+        {
+          rotateZ: `${rotate.value}deg`
+        },
+        {
+          scaleX: 10
+        },
+        {
+          scaleY: 1.5
+        },
+        {
+          translateY: -70
+        }
+      ]
+    };
+  });
+
+  const bottomStyle = useAnimatedStyle(() => {
+    const inputRange = messages.map((_, index) => -width * index).reverse();
+    const bgColor = interpolateColor(x.value, inputRange, bottomColors);
+
+    return {
+      backgroundColor: bgColor
+    };
+  });
+
+  const bottomStyle2 = useAnimatedStyle(() => {
+    const inputRange = messages.map((_, index) => -width * index).reverse();
+    const bgColor = interpolateColor(x.value, inputRange, bottomColors);
+
+    return {
+      backgroundColor: bgColor
+    };
+  });
+
+  const messageStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: x.value
+        }
+      ]
+    };
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={panGestureHandler}>
+      <Animated.View style={[styles.root, bottomStyle2]}>
+        <Animated.View
+          style={[StyleSheet.absoluteFill, styles.top, topStyle]}
+        />
+        <Animated.View
+          style={[StyleSheet.absoluteFill, styles.bottom, bottomStyle]}
+        />
+        <View style={styles.section}></View>
+        <View style={styles.section}>
+          <Animated.View style={[styles.message, messageStyle]}>
+            <View
+              style={[
+                StyleSheet.absoluteFill,
+                {
+                  flexDirection: 'row'
+                }
+              ]}
+            >
+              {messages.map((message, index) => {
+                return (
+                  <View
+                    key={index}
+                    style={{
+                      width,
+                      padding: ms(64)
+                    }}
+                  >
+                    <Text style={styles.title}>{message.title}</Text>
+                    <Text style={styles.subtitle}>{message.subtitle}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </Animated.View>
+          <View
+            style={{
+              padding: ms(64),
+              paddingTop: 0
+            }}
+          >
+            <Button3D text="Sign In" onPress={onSignInPress} />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginTop: ms(32)
+              }}
+            >
+              {messages.map((_, i) => {
+                // TODO: Create a component for this.
+                const style = useAnimatedStyle(() => ({
+                  backgroundColor: `rgba(255, 255, 255, ${
+                    index.value === i ? '1' : '0.5'
+                  })`
+                }));
+
+                return (
+                  <Animated.View
+                    key={i}
+                    style={[
+                      {
+                        height: 12,
+                        width: 12,
+                        borderRadius: 6,
+                        marginEnd: ms(6),
+                        marginStart: ms(6)
+                      },
+                      style
+                    ]}
+                  />
+                );
+              })}
+            </View>
+          </View>
+        </View>
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+const styles = ScaledSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: 'column',
+    overflow: 'hidden'
+  },
+  section: {
+    flex: 1,
+    overflow: 'visible'
+  },
+  top: {
+    bottom: '50%'
+  },
+  bottom: {
+    zIndex: -1,
+    top: '50%'
+  },
+  message: {
+    flex: 1
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: '16@ms',
+    color: 'white'
+  },
+  subtitle: {
+    fontStyle: 'italic',
+    fontSize: '14@ms',
+    color: 'white'
+  }
+});
+
+export default Onboarding;
--- pride/src/screens/SignInScreen/SignInScreen.tsx
+++ pride/src/screens/SignInScreen/SignInScreen.tsx
@@ -1,62 +1,38 @@
 // @refresh reset
+import useAuth from '@hooks/useAuth';
 import React from 'react';
 import { View } from 'react-native';
-import useAuth from '@hooks/useAuth';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ms } from 'react-native-size-matters';
-import Button3D from '@components/Button3D';
-import {
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent
-} from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedGestureHandler,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-  interpolateColor
-} from 'react-native-reanimated';
+import Onboarding from './Onboarding';
+
+const messages = [
+  {
+    title: 'Alentando la flama del amor.',
+    subtitle: 'Y de la verdad...'
+  },
+  {
+    title: 'OOOOOOOOOOOOOOO.',
+    subtitle: 'Y de la verdad...'
+  },
+  {
+    title: 'AAAAAAAAAAAAAAAAAAA.',
+    subtitle: 'AAAAAAAAAAAA...'
+  }
+];
 
 const SignInScreen: React.FC = () => {
-  const left = useSharedValue(false);
-  const value = useSharedValue(0);
-
   const { top } = useSafeAreaInsets();
 
   const { signIn } = useAuth();
 
-  const panGestureHandler =
-    useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
-      onEnd: e => {
-        left.value = e.translationX < 0;
-        value.value = withTiming(left.value ? 1 : 0);
-      }
-    });
-
-  const animatedStyle = useAnimatedStyle(() => {
-    const color = interpolateColor(value.value, [0, 1], ['yellow', 'blue']);
-
-    return {
-      backgroundColor: color
-    };
-  });
-
-  // TODO: Handle sign in error.
   return (
-    <View style={{ flex: 1, top }}>
-      <PanGestureHandler onGestureEvent={panGestureHandler}>
-        <Animated.View
-          style={[
-            animatedStyle,
-            {
-              padding: ms(16),
-              flex: 1
-            }
-          ]}
-        >
-          <Button3D text="Sign In" onPress={() => signIn()} />
-        </Animated.View>
-      </PanGestureHandler>
+    <View style={{ flex: 1, paddingTop: top }}>
+      <Onboarding
+        bottomColors={['#7F7B83', '#FE436D', '#2E409F']}
+        messages={messages}
+        topColors={['#26C0A4', '#FAFAFA', '#FBC02E']}
+        onSignInPress={() => signIn()}
+      />
     </View>
   );
 };
