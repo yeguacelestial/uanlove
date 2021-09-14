d0b363ea0e3a8e53c4d4e5c8fa71636f28ff6f2f
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-09-14 16:54:47 -0500

    :sparkles: Created Interest components

---
+++ pride/src/components/Interest/Interest.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { ScaledSheet } from 'react-native-size-matters';
+
+interface InterestProps {
+  text: string;
+  coincide?: boolean;
+  spacing?: number;
+  coincideColor?: string;
+  color?: string;
+}
+
+const Interest: React.FC<InterestProps> = ({
+  text = 'Music',
+  coincide = false,
+  spacing = 0,
+  coincideColor = '#fd546c',
+  color = '#667180'
+}: InterestProps) => {
+  return (
+    <View
+      style={[
+        styles.interest,
+        {
+          margin: spacing,
+          borderColor: coincide ? coincideColor : color
+        }
+      ]}
+    >
+      <Text
+        style={{
+          color: coincide ? coincideColor : color
+        }}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+};
+
+const styles = ScaledSheet.create({
+  interest: {
+    paddingHorizontal: '15@ms',
+    paddingVertical: '5@ms',
+    alignSelf: 'baseline',
+    borderWidth: 1,
+    backgroundColor: 'transparent',
+    borderRadius: '15@ms'
+  }
+});
+
+export default Interest;
---
+++ pride/src/components/Interest/index.tsx
@@ -0,0 +1,4 @@
+import Interest from './Interest';
+
+export * from './Interest';
+export default Interest;
