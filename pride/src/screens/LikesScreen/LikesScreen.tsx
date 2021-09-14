1c4e43d8d18563ca5e3554951bf2d2fcfafd7dc8
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-09-14 16:56:35 -0500

    :art: Added Interest components in likes screen

--- pride/src/screens/LikesScreen/LikesScreen.tsx
+++ pride/src/screens/LikesScreen/LikesScreen.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import ScreenScrollView from '@components/ScreenScrollView';
+import Interest from '@components/Interest/Interest';
+import { ms } from 'react-native-size-matters';
 
 const LikesScreen: React.FC = () => {
+  // TODO: Remove dummy data when real data is available.
+  const interests = [
+    { text: 'Music', coincide: true },
+    { text: 'Reading', coincide: false },
+    { text: 'Golfing', coincide: true },
+    { text: 'Party', coincide: true },
+    { text: 'Running', coincide: false }
+  ];
+
   return (
     <ScreenScrollView>
       <Text>Likes Screen</Text>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {interests.map(interest => (
+          <Interest
+            key={Math.random()}
+            coincide={interest.coincide}
+            spacing={ms(2)}
+            text={interest.text}
+          />
+        ))}
+      </View>
     </ScreenScrollView>
   );
 };
