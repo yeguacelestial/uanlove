b4ab5e282cf9a2fb18153fa6781dc95471cafc33
Johnatan Ramirez <johniliam2151@gmail.com>
2021-09-14 19:34:58 -0500

    3DButtonAnimation

--- pride/src/components/Button3D/Button3D.tsx
+++ pride/src/components/Button3D/Button3D.tsx
@@ -1,12 +1,20 @@
 // @refresh reset
-import React from 'react';
-import { Pressable, Text, PressableProps, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Pressable,
+  Text,
+  PressableProps,
+  View,
+  ActivityIndicator
+} from 'react-native';
 import { ScaledSheet, ms } from 'react-native-size-matters';
 import Animated, {
+  interpolateColor,
   useAnimatedStyle,
   useSharedValue,
   withTiming
 } from 'react-native-reanimated';
+import { FontAwesome } from '@expo/vector-icons';
 
 export interface Button3DProps extends PressableProps {
   text: string;
@@ -17,6 +25,7 @@ export interface Button3DProps extends PressableProps {
   backdropColor?: string;
   fontSize?: number;
   loading?: boolean;
+  success?: boolean;
 }
 
 // TODO: Add loading animation.
@@ -33,9 +42,12 @@ const Button3D: React.FC<Button3DProps> = ({
   style,
   onPressIn,
   onPressOut,
+  success,
   ...props
 }: Button3DProps) => {
   const translateY = useSharedValue(0);
+
+  const [bgColor, setbgColor] = useState({ backgroundColor, backdropColor });
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -47,10 +59,37 @@ const Button3D: React.FC<Button3DProps> = ({
     };
   });
 
+  // const animatedColor = useAnimatedStyle(() => {
+  //  const color = interpolateColor(
+  //  value.value, [0, 1], ['#fbc02e', 'green']);
+
+  //   return {
+  //     backgroundColor: color
+  //   };
+  // });
+
+  const loadingAnim = () => {
+    if (loading) {
+      return <ActivityIndicator color="#FFFFFF" size="small" />;
+    } else if (success) {
+      return <FontAwesome color="white" name="check" size={24} />;
+    } else {
+      return <Text style={[{ color, fontSize }, styles.text]}>{text}</Text>;
+    }
+  };
+
+  useEffect(() => {
+    if (success) {
+      setbgColor({ backgroundColor: '#52B542', backdropColor: '#539B47' });
+    }
+  }, [success]);
+
   return (
     <Pressable
       disabled={loading}
-      style={[style, { paddingBottom: backdropHeight }]}
+      //TODO: style={[style, { paddingBottom: backdropHeight }]}
+      //Da error en style
+      style={[{}, { paddingBottom: backdropHeight }]}
       onPressIn={e => {
         translateY.value = withTiming(backdropHeight, {
           duration: animationDuration
@@ -69,22 +108,18 @@ const Button3D: React.FC<Button3DProps> = ({
     >
       <Animated.View
         style={[
-          {
-            backgroundColor
-          },
+          { backgroundColor: bgColor.backgroundColor },
           styles.button,
           animatedStyle
         ]}
       >
-        <Text style={[{ color, fontSize }, styles.text]}>
-          {loading ? 'Loading...' : text}
-        </Text>
+        <View style={styles.textContainer}>{loadingAnim()}</View>
       </Animated.View>
       <View
         style={[
           {
             top: backdropHeight,
-            backgroundColor: backdropColor
+            backgroundColor: bgColor.backdropColor
           },
           styles.backdrop
         ]}
@@ -110,6 +145,9 @@ const styles = ScaledSheet.create({
     position: 'absolute',
     zIndex: -1,
     borderRadius: '5@ms'
+  },
+  textContainer: {
+    height: '17@ms'
   }
 });
 
