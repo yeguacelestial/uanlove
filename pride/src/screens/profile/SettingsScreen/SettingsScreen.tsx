2203e93d6317a60a37064ed4b5c56a4a94a84a91
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-09-21 18:42:05 -0500

    :art: Added settings components to SettingsScreen

--- pride/src/screens/profile/SettingsScreen/SettingsScreen.tsx
+++ pride/src/screens/profile/SettingsScreen/SettingsScreen.tsx
@@ -1,12 +1,45 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import Text from '@components/Text';
 import Setting from '@components/Setting';
-import React from 'react';
-import { Text, View } from 'react-native';
+import SettingValue from '@components/SettingValue';
+import SettingSwitch from '@components/SettingSwitch';
+import SettingArrow from '@components/SettingArrow';
+import SettingsContainer from '@components/SettingsContainer';
 
 const SettingsScreen: React.FC = () => {
+  const [value, setValue] = useState(false);
+
   return (
     <View>
-      <Setting label="KKTUA" />
-      <Setting label="KKTUA" />
+      <SettingsContainer title="Title">
+        <SettingValue label="Label" value="Test" />
+
+        <SettingSwitch
+          label="Switch"
+          value={value}
+          onValueChange={() => setValue(prev => !prev)}
+        />
+
+        <SettingArrow label="Arrow" separation={false} />
+      </SettingsContainer>
+      <SettingsContainer title="Title">
+        <SettingValue label="Label" value="Test" />
+
+        <SettingSwitch
+          label="Switch"
+          value={value}
+          onValueChange={() => setValue(prev => !prev)}
+        />
+
+        <SettingArrow label="Arrow" separation={false} />
+      </SettingsContainer>
+      <SettingsContainer spacing={0} title="Title">
+        <Setting label="KKTUA" separation={false}>
+          <Text>KKuate</Text>
+          <Text>Slider</Text>
+        </Setting>
+      </SettingsContainer>
     </View>
   );
 };
