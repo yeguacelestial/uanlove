5db3b9ba69f695795892c9804a1259f1ee09bf4f
Johnatan Ramirez <johniliam2151@gmail.com>
2021-09-23 15:20:59 -0500

    Range Lenght

--- pride/src/components/settings/SettingRange/SettingRange.tsx
+++ pride/src/components/settings/SettingRange/SettingRange.tsx
@@ -3,7 +3,6 @@ import Setting, { SettingProps } from '../Setting';
 import { ms } from 'react-native-size-matters';
 import Text from '@components/Text';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
-import { View } from 'react-native';
 
 export interface SettingRangeProps extends SettingProps {
   onValueChange?: (low: number, high: number) => void;
@@ -33,13 +32,17 @@ const SettingRange: React.FC<SettingRangeProps> = ({
       <MultiSlider
         enabledOne
         enabledTwo
-        containerStyle={{ alignItems: 'center' }}
+        containerStyle={{
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          height: ms(20)
+        }}
         isMarkersSeparated={true}
         markerStyle={{ backgroundColor: '#5783D7', height: 20, width: 20 }}
         max={100}
         min={18}
         selectedStyle={{ backgroundColor: '#5783D7' }}
-        sliderLength={ms(300)}
+        sliderLength={ms(350)}
         unselectedStyle={{ borderRadius: 100 }}
         values={[low, high]}
         onValuesChange={value => onValueChange(value[0], value[1])}
