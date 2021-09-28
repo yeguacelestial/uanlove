3e13abce5092ac02f394bea84f9615e4376c9e5d
Carlos Nava <yeguacelestial@gmail.com>
2021-09-27 20:21:51 -0500

    :wrench: azure ad integration: Update main file

--- affair/main.py
+++ affair/main.py
@@ -1,15 +1,15 @@
-# import graphene
+import graphene
 
-# import uvicorn
-# from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI
 
-# from starlette.graphql import GraphQLApp
+from starlette.graphql import GraphQLApp
 
-# class Query(graphene.ObjectType):
-#     hello = graphene.String(name=graphene.String(default_value="stranger"))
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
 
-#     def resolve_hello(self, info, name):
-#         return f"Hello {name}"
+    def resolve_hello(self, info, name):
+        return f"Hello {name}"
 
-# app = FastAPI()
-# app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
+app = FastAPI()
+app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
