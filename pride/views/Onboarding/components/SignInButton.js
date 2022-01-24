95117d626642f94c9b11358371fdabc65e99f0e6
Carlos Nava <yeguacelestial@gmail.com>
2022-01-23 21:48:25 -0600

    :sparkles: LOVE-17-feat(azure-connection): Integrate azure with expo auth session

--- pride/views/Onboarding/components/SignInButton.js
+++ pride/views/Onboarding/components/SignInButton.js
@@ -1,8 +1,13 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Button } from 'react-native';
+
 import AwesomeButtonC137 from 'react-native-really-awesome-button/src/themes/c137';
+
 import { MainStyles } from '../../../styles/core';
+import useAuthProvider from '../hooks/useAuthProvider';
 
 const SignInButton = () => {
+  const { promptAsync, affairResponse } = useAuthProvider();
+
   return (
     <View style={MainStyles.container}>
       <AwesomeButtonC137
@@ -12,7 +17,7 @@ const SignInButton = () => {
         height={50}
         borderRadius={5}
         onPress={(next) => {
-          // handlePressAsync();
+          promptAsync();
           next();
         }}
       >
