b6ed63be3a577e4203ec246dc06bb69aa8a10be3
Carlos Nava <yeguacelestial@gmail.com>
2022-02-09 00:58:49 -0600

    :fire: LOVE-28-delete(user-info-endpoint): Remove unused files

--- affair/users/views.py
+++ affair/users/views.py
@@ -1,5 +1,5 @@
 import email
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 
 from users.models import User
