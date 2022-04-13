6390cc7ff78cb7466ec67f11c0b8ca4dc56faae4
Carlos Nava <yeguacelestial@gmail.com>
2022-04-12 23:45:39 -0500

    :construction: LOVE-36-feat(expo-firebase-ntegration): Create take photo handler

--- pride/hooks/usePickImage.js
+++ pride/hooks/usePickImage.js
@@ -19,8 +19,75 @@ if (!getApps().length) {
   initializeApp(firebaseConfig);
 }
 
-// Firebase sets some timeers for a long period, which will trigger some warnings. Let's turn that off for this example
+// Firebase sets some timers for a long period, which will trigger some warnings.
 LogBox.ignoreLogs([`Setting a timer for a long period`]);
+
+const usePickImage = () => {
+  const [firebaseState, setFirebaseState] = useState({
+    image: null,
+    uploading: false
+  })
+  const [pickedImage, setPickedImage] = useState(null);
+
+  // If permissions are not granted, ask for them
+  useEffect(async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }, []);
+
+  // If user picks an image, upload it to firebase and get the URL
+  useEffect(async () => {
+    if (pickedImage) {
+      setFirebaseState({
+        loading: true,
+      });
+
+      const imageFirebaseUrl = await firebaseUploadImageAsync(pickedImage.uri)
+      setFirebaseState({
+        image: imageFirebaseUrl,
+        loading: false,
+      })
+    }
+  }, [pickedImage]);
+
+  const pickImage = async (reference) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setPickedImage({
+        reference,
+        uri: result.uri
+      });
+    }
+  }
+
+  const takePhoto = async (reference) => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    if (!result.cancelled) {
+      setPickedImage({
+        reference,
+        uri: result.uri
+      });
+    }
+  }
+
+  return { pickedImage, pickImage, takePhoto, firebaseState };
+};
 
 async function firebaseUploadImageAsync(uri) {
   // why use XMLHttpRequest instead of fetch?
@@ -47,55 +114,5 @@ async function firebaseUploadImageAsync(uri) {
 
   return await getDownloadURL(firebaseFileRef);
 }
-
-const usePickImage = () => {
-  const [firebaseParams, setFirebaseState] = useState({
-    image: null,
-    uploading: false
-  })
-  const [pickedImage, setPickedImage] = useState(null);
-
-  // componentDidMount
-  useEffect(async () => {
-    if (Platform.OS !== 'web') {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    }
-  }, []);
-
-  useEffect(async () => {
-    if (pickedImage) {
-      setFirebaseState({
-        ...firebaseParams,
-        loading: true,
-      });
-
-      const imageFirebaseUrl = await firebaseUploadImageAsync(pickedImage.uri)
-      alert(imageFirebaseUrl);
-    }
-  }, [pickedImage]);
-
-
-  const pickImage = async (reference) => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.cancelled) {
-      setPickedImage({
-        reference,
-        uri: result.uri
-      });
-    }
-  }
-
-  return { pickedImage, pickImage };
-};
 
 export default usePickImage;
